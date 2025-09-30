192a2b83ed9ff92373c280b3153ec3a8e1f0cb30
jkratochwill1 <jasonkratochwill1@gmail.com>
2025-09-30 15:28:05 -0400

    Enable CORS for frontend integration

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import os
 from flask import Flask, request, jsonify
 from utils.metadata_parser import extract_metadata
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/extract', methods=['POST'])
 def extract():
